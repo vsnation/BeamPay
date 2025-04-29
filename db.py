0484b02886e5188e5cd761426ac7888db0063c67
vsnation <vsnation.v@gmail.com>
2025-04-29 14:59:15 +0000

    updated indexes

--- db.py
+++ db.py
@@ -16,13 +16,22 @@ db = client[db_name]
 
 def update_indexes():
     # Create indexes for frequent queries
-    # Index for transactions by ID and status
-    db.txs.create_index([("_id", 1)], unique=True)  # Ensure transaction ID is unique
-    db.txs.create_index([("status", 1)])  # Index for transaction status
-    db.txs.create_index([("confirmations", 1)])  # Index for confirmations
-    db.txs.create_index([("asset_id", 1)])  # Index for asset_id
-    db.txs.create_index([("sender", 1)])
-    db.txs.create_index([("receiver", 1)])
+    db.txs.create_index([("create_time", -1)])  # Fast transaction sorting
+    db.txs.create_index([("sender", 1), ("receiver", 1)])  # Speed up address filtering
+    db.txs.create_index([("status", 1)])  # Faster status queries
+    db.txs.create_index([("asset_id", 1)])  # Queries based on asset types
+
+    db.addresses.create_index([("_id", 1)])  # Speed up balance lookups
+    db.addresses.create_index([("balance.available.0", -1)])  # Query BEAM balances faster
+    db.addresses.create_index([("balance.available", 1)])  # Faster balance queries
+
+    db.pending_withdrawals.create_index([("sender", 1), ("status", 1)])  # Pending withdrawals by sender
+    db.pending_withdrawals.create_index([("status", 1), ("create_time", 1)])  # Prioritize older withdrawals
+    db.pending_withdrawals.create_index([("asset_id", 1)])  # Query withdrawals by asset ID
+
+    db.payments.create_index([("user_id", 1)])  # Quick lookup of user payments
+    db.payments.create_index([("status", 1)])  # Fast filtering of payment statuses
+
 
     # Index for addresses
     db.addresses.create_index([("_id", 1)], unique=True)  # Ensure address ID is unique
